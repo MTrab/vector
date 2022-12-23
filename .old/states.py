62209ba6a0ee3833dec5ad8f72ac5bcd1daecc62
Malene Trab <malene@trab.dk>
2022-12-23 20:28:28 +0000

    Remove unused code

--- .old/states.py
+++
@@ -1,9 +0,0 @@
-"""For handling states."""
-from __future__ import annotations
-
-FEATURES_TO_IGNORE = ["NoFeature", "SDK"]
-STIMULATIONS_TO_IGNORE = ["PlacedOnCharger"]
-
-
-class VectorStates(dict):
-    """Handling Vector events."""
--- .old/vector_utils/exceptions.py
+++
@@ -1,5 +0,0 @@
-"""Vector util exceptions."""
-
-
-class VectorDatasetException(Exception):
-    """Representing an error fetching a dataset for Vector."""
